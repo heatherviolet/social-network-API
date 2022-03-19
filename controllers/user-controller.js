b18cb6f9cd85b560f76c69b74c33070f2e7a3057
Heather Violet <heatherviolet91@gmail.com>
2022-03-18 18:51:03 -0700

    basic user-controller created

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -18,9 +18,45 @@ const userController = {
     }, 
     // get one user by id and populated friend and thought data
     getOneUser({ params }, res) {
-        User.findOne
+        User.findOne({ _id: params.id })
+        .then(dbUserData => {
+            // if no user is found send 404
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No user found with this id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
     },
-    // post
+    // creat user 
+    createUser({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
+    },
+    // update user by id
+    updateUser({ params, body }, res) {
+        User.findOneandUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbUserData => {
+          if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+
+// delete user
+deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
+  }
 };
 
 module.exports = userController;
